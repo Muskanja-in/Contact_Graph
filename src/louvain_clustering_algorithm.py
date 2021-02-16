ce814f8aa5ea82eb4339c03bd26aaac8de11070b
Muskanja-in <65892572+Muskanja-in@users.noreply.github.com>
2021-02-16 13:45:32 +0530

    Update louvain_clustering_algorithm.py

--- src/louvain_clustering_algorithm.py
+++ src/louvain_clustering_algorithm.py
@@ -18,8 +18,8 @@ def community_detect(time_lower,time_upper):
   edgedata=gr.graphformation(time_lower,time_upper)[0]
   edge_data = pd.DataFrame(edgedata, columns=["node1", "node2","weight"])
   g = nx.Graph()
-  g = nx.from_pandas_edgelist(df, source='node1', target='node2', edge_attr ='weight',)
+  g = nx.from_pandas_edgelist(edge_data, source='node1', target='node2', edge_attr ='weight',)
   partition = community_louvain.best_partition(g)
   print(partition)
-  pg.plt_graph(partition,g)
+  pg.plt_graph(partition,edgedata)
   return partition
